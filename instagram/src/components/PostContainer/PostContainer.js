cc1eb7dbc4c406cc3a43170bc45cf284a441bad2
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-18 15:21:20 -0800

    Finished the search bar

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Post from './Post';
+import './Post.css';
+
+const PostContainer = props => {
+  return (
+    <div className="posts-container-wrapper">
+    </div>
+  );
+};
+
+export default PostContainer;
---
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import cameraLogo from '../../assets/camera-logo.svg';
+import igLogo from '../../assets/iglogo.png';
+import './SearchBarContainer.css'
+
+function SearchBarContainer(props) {
+    return (
+        <div className='search-bar-container'>
+            <div className="logos"> 
+                <img alt="camera logo" src={cameraLogo} className="camera-image" />
+                <img alt="instagram logo" src={igLogo} className="logo-image" />
+            </div>
+            
+            <input type="text" placeholder="Search" />
+
+            <div className="social-container">
+                <div className="social">
+                    <i className="fa fa-compass" />
+                </div>
+                <div className="social">
+                    <i className="fa fa-heart" />
+                </div>
+                <div className="social">
+                    <i className="fa fa-user-circle" />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default SearchBarContainer;
