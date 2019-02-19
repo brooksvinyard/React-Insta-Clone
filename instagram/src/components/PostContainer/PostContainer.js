1b9b53247ecb093784b5b0912929c217ced07f11
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-18 19:50:52 -0800

    MVP

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import Post from './Post';
+import Post from './Post';
 import './Post.css';
 
 const PostContainer = props => {
   return (
     <div className="posts-container-wrapper">
+      {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
     </div>
   );
 };
