16182e317a61ad1b9e195e462cdde8ca3a1e62b2
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-20 12:30:46 -0800

    Refactored front page into PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import SearchBarContainer from './components/SearchBar/SearchBarContainer'
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
+// import SearchBarContainer from './components/SearchBar/SearchBarContainer'
+// import dummyData from './dummy-data';
+// import PostContainer from './components/PostContainer/PostContainer';
+import PostsPage from './components/PostContainer/PostsPage';
 
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      posts: [],
-      searchPosts: []
+      // posts: [],
+      // searchPosts: []
     };
   }
 
-  componentDidMount() {
-    this.setState({ posts: dummyData })
-  }
+  // componentDidMount() {
+  //   this.setState({ posts: dummyData })
+  // }
 
-  searchPosts = e => {
-    const posts = this.state.posts.filter(p => {
-      if (p.username.includes(e.target.value)) {
-        return p;
-      }
-      return null;
-    });
-    this.setState({ searchPosts: posts });
-  };
+  // searchPosts = e => {
+  //   const posts = this.state.posts.filter(p => {
+  //     if (p.username.includes(e.target.value)) {
+  //       return p;
+  //     }
+  //     return null;
+  //   });
+  //   this.setState({ searchPosts: posts });
+  // };
 
   render() {
     return (
-      <div className="App">
-        <SearchBarContainer searchPosts={this.searchPosts} />
-        <PostContainer 
-        posts={this.state.searchPosts.length > 0 
-          ? this.state.searchPosts 
-          : this.state.posts} />
+      // <div className="App">
+      //   <SearchBarContainer searchPosts={this.searchPosts} />
+      //   <PostContainer 
+      //   posts={this.state.searchPosts.length > 0 
+      //     ? this.state.searchPosts 
+      //     : this.state.posts} />
         
-      </div>
+      // </div>
+      <PostsPage />
     );
   }
 }
